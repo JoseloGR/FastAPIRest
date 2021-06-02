cd496e0e44ffb8f3ebe40c5683129cb65145bf54
Jose Luis GR <A01063645@itesm.mx>
2021-06-01 23:39:33 -0500

    API Core

---
+++ db.py
@@ -0,0 +1,28 @@
+CLIENTS = [
+  {'id': 1, 'nombre': 'Cliente_1', 'usuario_id': 1, 'activo': 1},
+  {'id': 2, 'nombre': 'Cliente_2', 'usuario_id': 2, 'activo': 1}
+]
+
+USUARIOS = [
+  {'id': 1, 'nombre': 'Usuario_1', 'equipo_id': '1234567890', 'activo': 1},
+  {'id': 2, 'nombre': 'Usuario_3', 'equipo_id': '0987654321', 'activo': 2}
+]
+
+VENTAS = [
+  { 'id': 1, 'fecha': '2021-05-03 12:30:00', 'monto': 35000, 'cliente_id': 1, 'cliente_nombre': 'Cliente_1', 'usuario_id': 1, 'usuario_nombre': 'Usuario_1', 'activo': 1, 'equipo_id': 1, 'equipo_nombre': 'Equipo_1' },
+  { 'id': 2, 'fecha': '2021-05-04 12:30:00', 'monto': 20000, 'cliente_id': 1, 'cliente_nombre': 'Cliente_1', 'usuario_id': 2, 'usuario_nombre': 'Usuario_3', 'activo': 1, 'equipo_id': 2, 'equipo_nombre': 'Equipo_2' },
+  { 'id': 3, 'fecha': '2021-05-05 12:30:00', 'monto': 25000, 'cliente_id': 2, 'cliente_nombre': 'Cliente_2', 'usuario_id': 1, 'usuario_nombre': 'Usuario_1', 'activo': 1, 'equipo_id': 1, 'equipo_nombre': 'Equipo_1' },
+  { 'id': 4, 'fecha': '2021-05-06 12:30:00', 'monto': 11000, 'cliente_id': 2, 'cliente_nombre': 'Cliente_2', 'usuario_id': 2, 'usuario_nombre': 'Usuario_3', 'activo': 1, 'equipo_id': 2, 'equipo_nombre': 'Equipo_2' },
+  { 'id': 5, 'fecha': '2021-06-05 12:30:00', 'monto': 40000, 'cliente_id': 1, 'cliente_nombre': 'Cliente_1', 'usuario_id': 1, 'usuario_nombre': 'Usuario_1', 'activo': 1, 'equipo_id': 1, 'equipo_nombre': 'Equipo_1' },
+  { 'id': 6, 'fecha': '2021-07-06 12:30:00', 'monto': 10000, 'cliente_id': 2, 'cliente_nombre': 'Cliente_2', 'usuario_id': 2, 'usuario_nombre': 'Usuario_3', 'activo': 1, 'equipo_id': 2, 'equipo_nombre': 'Equipo_2' }
+]
+
+CAT_EQUIPOS = [
+  {'id': 1, 'nombre': 'Equipo_1'},
+  {'id': 2, 'nombre': 'Equipo_2'}
+]
+
+CAT_USUARIOS = [
+  {'id': 1, 'nombre': 'Usuario_1'},
+  {'id': 2, 'nombre': 'Usuario_3'}
+]
---
+++ main.py
@@ -0,0 +1,89 @@
+from fastapi import (
+  FastAPI,
+  status
+)
+from fastapi.responses import JSONResponse
+from db import (
+  CLIENTS,
+  USUARIOS,
+  VENTAS,
+  CAT_EQUIPOS,
+  CAT_USUARIOS
+)
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+  return {'version': '1.0.0'}
+
+
+@app.get("/clientes/{id}")
+def get_client_by_id(id: int):
+  client = list(filter(lambda x: x.get("id", 0) == id, CLIENTS))
+  response = {}
+  status_code = status.HTTP_404_NOT_FOUND
+  if client:
+    response = client[0]
+    status_code = status.HTTP_200_OK
+  return JSONResponse(
+    status_code=status_code,
+    content=response
+  )
+
+@app.get("/clientes")
+def get_clients():
+  return JSONResponse(
+    status_code=status.HTTP_200_OK,
+    content=CLIENTS
+  )
+
+@app.get("/usuarios/{id}")
+def get_user_by_id(id: int):
+  user = list(filter(lambda x: x.get("id", 0) == id, USUARIOS))
+  response = {}
+  status_code = status.HTTP_404_NOT_FOUND
+  if user:
+    response = user[0]
+    status_code = status.HTTP_200_OK
+  return JSONResponse(
+    status_code=status_code,
+    content=response
+  )
+
+@app.get("/usuarios")
+def get_users():
+  return JSONResponse(
+    status_code=status.HTTP_200_OK,
+    content=USUARIOS
+  )
+
+@app.get("/ventas/equipo/{id}")
+def get_sales_by_team(id: int):
+  sales = list(filter(lambda x: x.get("equipo_id", 0) == id, VENTAS))
+  status_code = status.HTTP_200_OK if sales else status.HTTP_404_NOT_FOUND
+  return JSONResponse(
+    status_code=status_code,
+    content=sales
+  )
+
+@app.get("/ventas/usuario/{id}")
+def get_sales_by_user(id: int):
+  sales = list(filter(lambda x: x.get("usuario_id", 0) == id, VENTAS))
+  status_code = status.HTTP_200_OK if sales else status.HTTP_404_NOT_FOUND
+  return JSONResponse(
+    status_code=status_code,
+    content=sales
+  )
+
+@app.get("/catalogo/{nombre}")
+def get_catalog(nombre: str):
+  options = {
+    'equipos': CAT_EQUIPOS,
+    'usuarios': CAT_USUARIOS
+  }
+  catalogs = options.get(nombre, [])
+  return JSONResponse(
+    status_code=status.HTTP_200_OK,
+    content=catalogs
+  )
